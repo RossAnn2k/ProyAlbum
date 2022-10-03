aa082b700f8bdc6cd675889c28251b2fd0f72344
RossAnn2k <114898225+RossAnn2k@users.noreply.github.com>
2022-10-03 18:47:52 -0500

    Update CreaSPConsultaFoto.sql

--- CreaSPConsultaFoto.sql
+++ CreaSPConsultaFoto.sql
@@ -1,5 +1,5 @@
 SET SERVEROUTPUT ON;
-create or replace procedure AltaFoto (incodigo in number) is
+create or replace procedure spConsultaFoto (incodigo in number) is
 vdescrip FOTOGRAFIA.DESCRIPCION%TYPE;
 vclasific FOTOGRAFIA.CLASIFICACION%TYPE;
 
