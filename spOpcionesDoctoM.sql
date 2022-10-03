8d19fcf76f974ef7cab6219e2c34b6b9030426ec
RossAnn2k <114898225+RossAnn2k@users.noreply.github.com>
2022-10-03 17:30:22 -0500

    Add files via upload

---
+++ spOpcionesDoctoM.sql
@@ -0,0 +1,21 @@
+
+create or replace PROCEDURE opcionesDoctoM( 
+FOL in	DOCTOFOTOGRAFICO.FOLIO%TYPE,
+CLASIF	in DOCTOFOTOGRAFICO.CLASIFICACION%TYPE,
+COMEN in DOCTOFOTOGRAFICO.COMENTARIO%TYPE
+)
+IS
+  -- Declaración de variables locales
+SET SERVEROUTPUT ON;  
+  BEGIN
+  -- Instrucciones de ejecución
+    BEGIN
+        update DOCTOFOTOGRAFICO set comentario=comen where FOLIO = FOL;
+        commit;
+        DBMS_OUTPUT.PUT_LINE('Documento modificado');
+    EXCEPTION
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('Upps hubo un ERROR');
+    END;
+    
+END;
